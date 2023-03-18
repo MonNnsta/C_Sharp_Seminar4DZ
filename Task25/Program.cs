c43edca19669609c8858853ed8321cadf079702d
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-03-18 16:40:21 +0500

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Stepen(int numA, int numB)
+{
+  int res = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    res = res * numA;
+  }
+    return res;
+}
+
+  Console.Write("Input A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Input B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Stepen(numA, numB);
+  Console.WriteLine("Solution = "+ exponentiation);
